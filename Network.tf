83cc2b38783ced539bc62a907a6f2acacc34dd0a
Vijay097 <yodharth@gmail.com>
2026-05-31 18:41:00 +0530

    first commit

---
+++ Network.tf
@@ -0,0 +1,175 @@
+# VPC
+resource "aws_vpc" "Projectvpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+  region = "ap-south-1"
+
+  tags = {
+    Name = "Projectvpc"
+  }
+}
+
+# subnets
+resource "aws_subnet" "Public-1" {
+  vpc_id     = aws_vpc.Projectvpc.id
+  cidr_block = "10.0.0.0/25"
+  region = "ap-south-1"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "Public-1"
+  }
+}
+resource "aws_subnet" "Public-2" {
+  vpc_id     = aws_vpc.Projectvpc.id
+  cidr_block = "10.0.0.128/26"
+  region = "ap-south-1"
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name = "Public-2"
+  }
+}
+
+resource "aws_subnet" "Private-1" {
+  vpc_id     = aws_vpc.Projectvpc.id
+  cidr_block = "10.0.1.0/25"
+  region = "ap-south-1"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "Private-1"
+  }
+}
+resource "aws_subnet" "Private-2" {
+  vpc_id     = aws_vpc.Projectvpc.id
+  cidr_block = "10.0.1.128/25"
+  region = "ap-south-1"
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name = "Private-2"
+  }
+}
+
+# internet gateway
+resource "aws_internet_gateway" "Project_igw" {
+  vpc_id = aws_vpc.Projectvpc.id
+
+  tags = {
+    Name = "Project_igw"
+  }
+}
+
+# NAT gateway
+resource "aws_nat_gateway" "Project_nat_gw" {
+  allocation_id = aws_eip.project_eip.id
+  subnet_id     = aws_subnet.Public-1.id
+
+  tags = {
+    Name  = "Project_nat_gw"
+
+  }
+  depends_on = [aws_internet_gateway.Project_igw]
+}
+
+# elastic ip for nat gateway
+resource "aws_eip" "project_eip" {
+  domain = "vpc"
+  tags = {
+    Name  = "project_eip"
+  }
+}
+# Route table for public subnets
+resource "aws_route_table" "rt_public" {
+  vpc_id = aws_vpc.Projectvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.Project_igw.id
+  }
+
+  tags = {
+    Name  = "rt_public"
+  }
+}
+resource "aws_route_table_association" "rta_public_1" {
+  subnet_id      = aws_subnet.Public-1.id
+  route_table_id = aws_route_table.rt_public.id
+}
+resource "aws_route_table_association" "rta_public_2" {
+  subnet_id      = aws_subnet.Public-2.id
+  route_table_id = aws_route_table.rt_public.id
+}
+
+
+# Route table for private subnets
+resource "aws_route_table" "rt_private" {
+  vpc_id = aws_vpc.Projectvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.Project_nat_gw.id
+  }
+
+  tags = {
+    Name  = "rt_private"
+  }
+}
+
+resource "aws_route_table_association" "rta_private_1" {
+  subnet_id      = aws_subnet.Private-1.id
+  route_table_id = aws_route_table.rt_private.id
+}
+resource "aws_route_table_association" "rta_private_2" {
+  subnet_id      = aws_subnet.Private-2.id
+  route_table_id = aws_route_table.rt_private.id
+}
+
+resource "aws_security_group" "Private_sg" {
+  name        = "Private_sg"
+  description = "Private security group"
+  vpc_id      = aws_vpc.Projectvpc.id
+
+  tags = {
+    Name = "Private_sg"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.Private_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.Private_sg.id
+  #cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+  referenced_security_group_id = aws_security_group.ALB_sg.id
+}
+
+resource "aws_security_group" "ALB_sg" {
+  name        = "ALB_sg"
+  description = "ALB security group"
+  vpc_id      = aws_vpc.Projectvpc.id
+
+  tags = {
+    Name = "ALB_sg"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
+  security_group_id = aws_security_group.ALB_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls" {
+  security_group_id = aws_security_group.ALB_sg.id
+  cidr_ipv4         = "0.0.0.0/0" 
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
---
+++ Server.tf
@@ -0,0 +1,83 @@
+resource "aws_instance" "web_server-1" {
+  ami           = "ami-0685bcc683dadb6b9" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.Private-1.id
+  #key_name      = "vijay-key-pair"
+  vpc_security_group_ids = [aws_security_group.Private_sg.id]
+  user_data = file("user_data.sh")
+  #associate_public_ip_address = true
+
+  tags = {
+    Name = "WebServer-1"
+  }
+}
+resource "aws_instance" "web_server-2" {
+  ami           = "ami-0685bcc683dadb6b9" # Amazon Linux 2 AMI (HVM), SSD Volume Type
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.Private-2.id
+  #key_name      = "vijay-key-pair"
+  vpc_security_group_ids = [aws_security_group.Private_sg.id]
+  user_data = file("user_data.sh")
+  #associate_public_ip_address = true
+    
+
+  tags = {
+    Name = "WebServer-2"
+  }
+}
+
+resource "aws_lb_target_group" "Project_tg" {
+  name     = "Project-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.Projectvpc.id
+  health_check {
+    
+  healthy_threshold   = 2
+  unhealthy_threshold = 2
+  timeout             = 3
+  interval            = 30
+  path                = "/"
+  port                = "80"
+  protocol            = "HTTP"
+  matcher             = "200"
+
+  }
+  
+}
+
+resource "aws_lb_target_group_attachment" "Project_tg_attachment" {
+  for_each         = {
+    web_server_1 = aws_instance.web_server-1.id
+    web_server_2 = aws_instance.web_server-2.id
+  }
+
+  target_group_arn = aws_lb_target_group.Project_tg.arn
+  target_id        = each.value
+  port             = 80
+}
+
+resource "aws_lb" "Project_alb" {
+  name               = "Project-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ALB_sg.id]
+  subnets            = [aws_subnet.Public-1.id, aws_subnet.Public-2.id]
+
+  tags = {
+    Name = "Project-alb"
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.Project_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  #ssl_policy        = "ELBSecurityPolicy-2016-08"
+  #certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.Project_tg.arn
+  }
+}
