75e24184690d8e319802709bc3adb8a2b30470f3
Vijay097 <yodharth@gmail.com>
2026-06-01 17:49:56 +0530

    second commit-1

--- microservice-gitops-cicd-pipeline/userJenkins_data.sh
+++ microservice-gitops-cicd-pipeline/userJenkins_data.sh
@@ -33,3 +33,18 @@ usermod -aG docker jenkins
 systemctl restart jenkins
 
 echo "================ BOOTSTRAP COMPLETE ================"
+echo "================ STARTING BOOTSTRAP (AMAZON LINUX) ================"
+
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# 4. Install Minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+install minikube-linux-amd64 /usr/local/bin/minikube
+
+# 5. Start Minikube SAFELY as the 'ec2-user'
+# This ensures config files are generated in /home/ec2-user/.kube/
+su - ec2-user -c "minikube start --driver=docker"
+
+echo "================ BOOTSTRAP COMPLETE ================"
